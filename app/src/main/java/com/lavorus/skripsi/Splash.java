34273a4a3ee0893a170f71b5a98565aecd75d77b
Alvin Leonardo <alvin@lavorus.com>
2017-01-22 10:41:39 +0700

    Add splash, logo, and fix some bugs

---
+++ app/src/main/java/com/lavorus/skripsi/Splash.java
@@ -0,0 +1,21 @@
+package com.lavorus.skripsi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+public class Splash extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int secondsDelayed = 2;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
+    }
+}
